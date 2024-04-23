0067172f6cff1c45b03d643ef3a9fdb74f61f5c9
Kainat Najmudin <167628536+kainatnayab@users.noreply.github.com>
2024-04-23 07:53:06 -0700

    Console Application
    Create a Console Application in C# to input distance of object from lens (p) and distance of image from lens (q). Find and print
    the focal length of the lens (f) using formula 1/f = /1p + /1q.

---
+++ console app 06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem___06_console_app_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            double p, q, f;
+
+            Console.WriteLine("Enter distance of object from lens:");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter distance of image from lens:");
+            q = Convert.ToDouble(Console.ReadLine());
+
+            //fomrula
+
+            f = p * q / p + q;
+
+            Console.WriteLine($"The focal length of the lens is {f}.");
+            Console.ReadLine();
+        }
+    }
+}
