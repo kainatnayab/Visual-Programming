7fc8a1d4db0fcf4eb5298c887e190a1e496b1965
Kainat Najmudin <167628536+kainatnayab@users.noreply.github.com>
2024-04-23 07:57:40 -0700

    Console Application
    Create a Console Application in C# to input a five-digit number. Find and print the sum of its digits. For example, if the input is
    12345, then sum of its digits will be 1+2+3+4+5=15. Don’t use any loop.

---
+++ console app 09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem___09_console_app_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            int n1, n2, n3, n4, n5,sum;
+
+            Console.WriteLine("Enter 1st number:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number:");
+            n2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3rd number:");
+            n3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 4th number:");
+            n4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 5th number:");
+            n5 = Convert.ToInt32(Console.ReadLine());
+
+            //sum of five numbers
+            sum = n1 + n2 + n3 + n4 + n5;
+            //for output
+            Console.WriteLine($"The sum of any five numbers {n1} + {n2} + {n3} + {n4} + {n5} is {sum}.");
+            Console.ReadLine();
+        }
+    }
+}
