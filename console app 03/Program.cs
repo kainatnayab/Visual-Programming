e157b9b5e33b1e04c7828ff9315b49d6126cfa0b
Kainat Najmudin <167628536+kainatnayab@users.noreply.github.com>
2024-04-23 07:45:56 -0700

    Console Application
    Create a Console Application in C# to input length and width of a rectangle. Find and print its area and perimeter.

---
+++ console app 03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem___03_console_app0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration of variables
+            double length, width, a, p;
+
+            Console.WriteLine("Length of a rectangle:");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Width of a rectangle:");
+            width = Convert.ToDouble(Console.ReadLine());
+            //calculate area and perimeter
+            a = length * width;
+            p = 2 * (length + width);
+
+            Console.WriteLine($"The Area of a rectangle is {a}.");
+            Console.WriteLine($"The Perimeter of a rectangle is {p}.");
+            Console.ReadLine();
+        }
+    }
+}
